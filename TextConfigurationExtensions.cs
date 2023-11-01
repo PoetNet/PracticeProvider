28c88a55351916b86871b79d3310e52b6eb0c9ba
marfutin <130133417+marfutin@users.noreply.github.com>
2023-11-01 12:12:14 +0300

    initial commit

---
+++ TextConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+public static class TextConfigurationExtensions
+{
+    public static IConfigurationBuilder AddTextFile(this IConfigurationBuilder builder, 
+        string path)   
+    {
+        if (builder == null)
+        { throw new ArgumentNullException(nameof(builder)); }
+        if (string.IsNullOrEmpty(path))
+        { throw new ArgumentException("Path to the file is not specified"); }
+
+        var source = new TextConfigurationSource(path);
+        builder.Add(source);
+        return builder;
+    }
+}
---
+++ TextConfigurationSource.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Configuration;
+
+public class TextConfigurationSource : IConfigurationSource
+{
+    public string FilePath { get; }
+    public TextConfigurationSource(string filename)
+    {
+        FilePath = filename;
+    }
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        // получаем полный путь для файла
+        string filePath = builder.GetFileProvider().GetFileInfo(FilePath).PhysicalPath;
+        return new TextConfigurationProvider(filePath);
+    }
+}
